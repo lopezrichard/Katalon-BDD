133613ef8f6eea8f38b45e81c29e20a6fb4c9403
lopezrichard <84924555+lopezrichard@users.noreply.github.com>
2024-05-28 21:22:01 -0400

    addd suite

--- Scripts/test/Script1716945375516.groovy
+++ Scripts/test/Script1716945375516.groovy
@@ -16,4 +16,6 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-
+WebUI.openBrowser("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+WebUI.takeFullPageScreenshot()
+WebUI.closeBrowser()
---
+++ Test Suites/SuiteTest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a8ab8036-c287-43a5-a64e-791101d891bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>f2218e0c-4208-4808-bdad-65abbe1fc0fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
